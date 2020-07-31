8554e8b28fb83aba9b09d6c84f925438cb6c197d
Michal Geci <michal.geci.1995@gmail.com>
2020-07-31 18:32:39 +0200

    Add release build, edit applicationId

--- app/src/androidTest/java/sk/ferinaf/covidskstats/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/sk/ferinaf/covidskstats/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("sk.ferinaf.myapplication", appContext.packageName)
+        assertEquals("sk.ferinaf.covid19SKStats", appContext.packageName)
     }
 }
